3e96793b1f6322471b69f225cf69eb3eb1d858e4
AlexDeusdete <alexdeusdete@hotmail.com>
2020-11-29 16:28:03 -0300

    Criação da Class App que faz o registro dos servicos e ajuste na CatalogViewModel

---
+++ XPedido/App.cs
@@ -0,0 +1,25 @@
+﻿using MvvmCross;
+using MvvmCross.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using XPedido.Interfaces;
+using XPedido.Services;
+using XPedido.ViewModels;
+
+namespace XPedido
+{
+    public class App : MvxApplication
+    {
+        public override void Initialize()
+        {
+            Mvx.IoCProvider.RegisterSingleton<IApi>(() => new Api());
+            Mvx.IoCProvider.RegisterType<ICategoryService, CategoryService>();
+            Mvx.IoCProvider.RegisterType<IOrderService, OrderService>();
+            Mvx.IoCProvider.RegisterType<IProductService, ProductService>();
+            Mvx.IoCProvider.RegisterType<IProductPromotionService, ProductPromotionService>();
+
+            RegisterAppStart<CatalogViewModel>();
+        }
+    }
+}
--- XPedido/ViewModels/CatalogViewModel.cs
+++ XPedido/ViewModels/CatalogViewModel.cs
@@ -32,9 +32,26 @@ namespace XPedido.ViewModels
         public override async Task Initialize()
         {
             await base.Initialize();
+            await PopulateProducts();
 
             _total = 0;
         }
+
+        public MvxObservableCollection<Product> Products => new MvxObservableCollection<Product>();
+
+        public async Task PopulateProducts()
+        {
+            try
+            {
+                List<Product> products = await _productService.GetProducts();
+                products.ForEach(x => products.Add(x));
+            }
+            catch (Exception exception)
+            {
+                System.Diagnostics.Debug.WriteLine(exception.Message);
+            }                        
+        }
+
 
         private double _total;
         public double Total 
@@ -62,7 +79,7 @@ namespace XPedido.ViewModels
             if (_order == null)
                 _order = await _orderService.CreateOrder();
 
-            if (_order.GetOrderProducts().Any(x => x.Product.Id == product.Id))
+            if (_order.GetOrderProduct(product) != null)
                 _order.GetOrderProduct(product).IncrementDecrementQuantity(1);
             else
                 _order.AddProduct(product, 1, await _productPromotionService.GetProductPromotionByCategoryId(product.Category_id.Value));
@@ -80,7 +97,7 @@ namespace XPedido.ViewModels
 
         private void DecrementProductQuantity(Product product)
         {
-            if (_order.GetOrderProducts().Any(x => x.Product.Id == product.Id))
+            if (_order.GetOrderProduct(product) != null)
                 _order.GetOrderProduct(product).IncrementDecrementQuantity(1);
         }
     }
