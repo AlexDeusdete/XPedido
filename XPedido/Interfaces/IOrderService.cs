34e9ebb52988bc9382476db88d3cb8cc540cf64f
AlexDeusdete <alexdeusdete@hotmail.com>
2020-11-28 22:44:47 -0300

    Serviço de Pedidos

---
+++ XPedido/Interfaces/IOrderService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using XPedido.Models;
+
+namespace XPedido.Interfaces
+{
+    public interface IOrderService
+    {
+        Task<List<Order>> GetOrders();
+        Task<Order> GetOrderById();
+        Task<Order> CreateOrder();
+
+        Task<bool> FinalizeOrder();
+    }
+}
---
+++ XPedido/Services/OrderService.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using XPedido.Interfaces;
+using XPedido.Models;
+
+namespace XPedido.Services
+{
+    public class OrderService : IOrderService
+    {
+        public async Task<Order> CreateOrder()
+        {
+            return new Order(1);
+        }
+
+        public async Task<bool> FinalizeOrder()
+        {
+            return true;
+        }
+
+        public Task<Order> GetOrderById()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<Order>> GetOrders()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
